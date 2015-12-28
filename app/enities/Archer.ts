fd0c6cf1272941c0edf83cd4ae427a9441724a20
Nanne Wielinga <nanne@mycel.nl>
2015-12-28 16:51:46 +0100

    add archer, add mage

--- app/enities/Archer.ts
+++ app/enities/Archer.ts
@@ -1,6 +1,22 @@
 module TD {
-  class Archer implements TD.Position {
+  export class Archer implements TD.Position, TD.BoundingBox, TD.Draw {
     x: number;
     y: number;
+    width: number;
+    height: number;
+    image: HTMLImageElement;
+
+    constructor(pos: TD.Position) {
+      this.width = 30;
+      this.height = 30;
+
+      this.x = pos.x - Math.floor(this.width / 2);
+      this.y = pos.y - Math.floor(this.height / 2);
+
+      this.image = <HTMLImageElement>document.getElementById("imgArcher");
+    }
+    draw(ctx) {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    };
   }
 }
