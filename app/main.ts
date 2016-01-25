7b4f72195100e76cccf402d70c4f6984e128eb6a
Sander Beekhuis <sander.beekhuis@live.nl>
2016-01-25 22:19:31 +0100

    Fixup of beter selection

--- app/main.ts
+++ app/main.ts
@@ -185,9 +185,12 @@ module TD {
         var thing = this.things[i];
 
         var xRange = new TD.Range(thing.x-Math.floor(thing.width/2), thing.x + Math.floor(thing.width/2));
-        var yRange = new TD.Range(thing.x-Math.floor(thing.height/2), thing.x + Math.floor(thing.height/2));
+        var yRange = new TD.Range(thing.y-Math.floor(thing.height/2), thing.y + Math.floor(thing.height/2));
+
+        console.log(mouseX, mouseY);
 
         if (xRange.constains(mouseX) && yRange.constains(mouseY)) {
+          console.log("bingo", thing)
           if (this.selected.contains(thing)) {
             this.selected.remove(thing);
             thing.select(false);
